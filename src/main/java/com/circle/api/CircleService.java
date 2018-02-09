6de7b7cbcf0e382b45fd789a1e17a3a73d4237f5
Toni Sutomo <toni.sutomo@gmail.com>
2018-02-09 09:38:29 +0700

    add post

--- src/main/java/com/circle/api/CircleService.java
+++ src/main/java/com/circle/api/CircleService.java
@@ -6,17 +6,26 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
  
 @Path("/circle")
 public class CircleService {
 
-	@GET
-    @Path("/{name}")
+	@GET 
+	@Path("/{name}")
     @Produces(MediaType.APPLICATION_JSON)
     public Circle produceJSON( @PathParam("name") String name ) {
         Circle st = new Circle(name, "Sutomo", 22, 1);
         return st;
     }	
+	
+	@POST
+	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	public Response createJsonCircle(Circle circle, @Context UriInfo uriInfo) {
+		Circle newCircle = new Circle(circle.getFirstName(), circle.getLastName(), 22, 2);
+		return Response.status(Response.Status.CREATED.getStatusCode()).header("Location", String.format("%s/%s", uriInfo.getAbsolutePath().toString(), newCircle.getId())).build();
+	}
 }
